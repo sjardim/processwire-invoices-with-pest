f713e56398e6eaff5e83994b057b519728679142
Sérgio Jardim <sergio@sergiojardim.com>
2025-02-21 17:17:31 +0000

    Update AboutUsPageTest.php
    Adding status check

--- tests/Feature/AboutUsPageTest.php
+++ tests/Feature/AboutUsPageTest.php
@@ -1,10 +1,15 @@
 <?php
 
+use ProcessWire\Page;
+
 test('about us page exists and is accessible', function () {
     $aboutpage = pages()->get('/about-us');
     
     expect($aboutpage)->toBeProcessWirePage()
         ->and($aboutpage->parent->id)->toBe(1)
+        // status(true) returns an array of status names assigned to page
+        // we expect it to be empty, meaning the page is published and not hidden
+        ->and($aboutpage->status(true))->toBeEmpty() 
         ->and($aboutpage->template->name)->toBe('basic-page');
 });
 
